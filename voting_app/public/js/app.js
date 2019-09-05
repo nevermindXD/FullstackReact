88c90084f88e5c755ad06e341c07969c3b039a34
Juan Carlos Olivier Jasso <juanc.olivierj@gmail.com>
2019-09-05 14:38:24 -0700

    state and props chapter 1

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,6 +1,39 @@
 class ProductList extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            products:[]
+        }
+
+        this.handleProductUpVote = this.handleProductUpVote.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({products: Seed.products})
+    }
+
+    handleProductUpVote(productId) {
+        const nextProducts = this.state.products.map((product) => {
+            if(product.id === productId){
+                    return Object.assign({}, product,{
+                        votes: product.votes + 1
+                    })
+            }else{
+                return product;
+            }
+        });
+
+        this.setState({
+            products: nextProducts
+        })
+      }
+    
+    
     render(){
-        const productComponents = Seed.products.map((product) => (
+        const products = this.state.products.sort((a,b) => (b.votes - a.votes));
+
+        const productComponents = products.map((product) => (
                 <Product
                     key={`product-${product.id}`}
                     id={product.id}
@@ -10,6 +43,7 @@ class ProductList extends React.Component {
                     votes={product.votes}
                     submitterAvatarUrl={product.submitterAvatarUrl}
                     productImageUrl={product.productImageUrl}
+                    onVote={this.handleProductUpVote}
                 />
         ));
         return(
@@ -19,8 +53,17 @@ class ProductList extends React.Component {
         );
     }
 }
-//55
+
 class Product extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleUpVote = this.handleUpVote.bind(this)
+    }
+    
+    handleUpVote(){
+        this.props.onVote(this.props.id);
+    }
+
     render() { 
         const {id, title, description, url, votes, submitterAvatarUrl, productImageUrl} = this.props;
         return(
@@ -30,7 +73,7 @@ class Product extends React.Component {
                 </div>
                 <div className='middle aligned content'>
                     <div className='header'>
-                        <a>
+                        <a onClick={this.handleUpVote}>
                             <i className='large caret up icon'/>
                         </a>
                         {votes}
