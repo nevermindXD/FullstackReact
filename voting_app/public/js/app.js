5ac529df47d45931b7eb95bf17c60d9d06333ace
Juan Carlos Olivier Jasso <juanc.olivierj@gmail.com>
2019-09-06 07:59:31 -0700

    refactor chapter 1

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,19 +1,13 @@
 class ProductList extends React.Component {
-    constructor(props){
-        super(props);
-
-        this.state = {
-            products:[]
-        }
-
-        this.handleProductUpVote = this.handleProductUpVote.bind(this);
+    state = {
+         products:[]
     }
 
     componentDidMount() {
         this.setState({products: Seed.products})
     }
 
-    handleProductUpVote(productId) {
+    handleProductUpVote = (productId) => {
         const nextProducts = this.state.products.map((product) => {
             if(product.id === productId){
                     return Object.assign({}, product,{
@@ -57,10 +51,10 @@ class ProductList extends React.Component {
 class Product extends React.Component {
     constructor(props){
         super(props);
-        this.handleUpVote = this.handleUpVote.bind(this)
+        // this.handleUpVote = this.handleUpVote.bind(this)
     }
     
-    handleUpVote(){
+    handleUpVote = () => {
         this.props.onVote(this.props.id);
     }
 
